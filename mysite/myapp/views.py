79b6a2f37d394e51553a8e1c9c63e231b40bb706
farxadovich <abbosmahamedov4@gmail.com>
2021-07-01 12:35:59 +0300

    test bec

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -12,3 +12,13 @@ def test(request, category):
     categ1 = Category.objects.get(pk=category)
     questions = Question.objects.filter(category=categ1)
     return render(request, 'myapp/test.html', {'questions': questions})
+
+
+def check(request):
+    print(request.GET)
+    result = ''
+    for key, value in request.GET.items():
+        choice = Choice.objects.get(pk=value)
+        print(choice)
+        result = result + f"For Question {key},answer is {choice.correct}<br>"
+    return HttpResponse(result)
